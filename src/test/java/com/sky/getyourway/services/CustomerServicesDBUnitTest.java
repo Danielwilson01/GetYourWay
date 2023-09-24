59123084aae56e48d8716bb4e505e1c63a37be5f
Sara Bonnin <saesbori@gmail.com>
2023-09-24 11:51:16 +0100

    add skeleton for CustomerDB test

---
+++ src/test/java/com/sky/getyourway/services/CustomerServicesDBUnitTest.java
@@ -0,0 +1,22 @@
+//package com.sky.getyourway.services;
+//
+//import com.sky.getyourway.repo.CustomerRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.context.ActiveProfiles;
+//
+//import java.util.Optional;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ActiveProfiles("test")
+//public class CustomerServicesDBUnitTest {
+//
+//    @Autowired
+//    private CustomerService service;
+//
+//    @MockBean
+//    private CustomerRepo repo;
+//
+//
+//}
